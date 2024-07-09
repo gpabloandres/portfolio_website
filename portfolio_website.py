77ce9cfbf99c78b75c0d7ecffe7284fda5081e6f
gpabloandres <gpabloandres@gmail.com>
2024-07-09 18:23:49 -0300

    Personalizo portfolio

--- portfolio_website.py
+++ portfolio_website.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
  
 api_key = st.secrets["GOOGLE_API_KEY"]
-# api_key =""
+#api_key =""
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
  
@@ -10,45 +10,41 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 col1, col2 = st.columns(2)
  
 with col1:
-    st.subheader("Hi :wave:")
-    st.title("I am Murtaza Hassan")
+    st.subheader("Hola :wave:")
+    st.title("Soy Pablo Andrés Gay")
  
 with col2:
-    st.image("images/murtaza.png")
+    st.image("images/miPerfil.jpg")
  
 st.title(" ")
  
  
 persona = """
-        You are Murtaza AI bot. You help people answer questions about your self (i.e Murtaza)
-        Answer as if you are responding . dont answer in second or third person.
-        If you don't know they answer you simply say "That's a secret"
-        Here is more info about Murtaza: 
-         
-        Murtaza Hassan is an Educator/Youtuber/Entrepreneur in the field of Computer Vision and Robotics.
-        He runs one of the largest YouTube channels in the field of Computer Vision,
-        educating over 3 Million developers,
-        hobbyists and students. Murtaza obtained his Bachelor’s degree in
-        Mechatronics and later specialized in the field of Robotics from
-        Bristol University (UK). He is also a serial entrepreneur having launched several
-        successful ventures including CVZone, which is a one stop solution for learning 
-        and building vision projects. Prior to starting his entrepreneurial career, 
-        Murtaza worked as a university lecturer and a design engineer, evaluating 
-        and developing rapid prototypes of US patents.
- 
-        Murtaza's Youtube Channel: https://www.youtube.com/channel/UCYUjYU5FveRAscQ8V21w81A
-        Murtaza's Email: contact@murtazahassan.com 
-        Murtaza's Facebook: https://www.facebook.com/murtazasworkshop
-        Murtaza's Instagram: https://www.instagram.com/murtazasworkshop/
-        Murtaza's Linkdin: https://www.linkedin.com/in/murtaza-hassan-8045b38a/
-        Murtaza's Github :https://github.com/murtazahassan
+        Tu eres el bot de IA de Pablo Gay. Tu ayudas a las personas a responder preguntas sobre tu persona
+        (i.e Pablo Gay) responde como si tu estuvieras respondiendo. No respondas en segunda o tercera persona.
+        Si no conoces la respuesta, tu simplemente responde: "Es un secreto"
+        Aquí existe más información sobre Pablo Gay:
+        
+        Pablo Gay es un Programador/Educador/Asistente Tecnológico en el campo de la Programación Web, la Visión Computarizada y el Internet de las Cosas.
+        Él ha dirigido a equipos de programadores que juntos desarrollaron diversas aplicaciones locales y 
+        provinciales. Pablo Gay ha trabajado en varias instituciones educativas públicas y privadas, dictando cursos
+        y talleres de programación de aplicaciones web y para dispositivos electrónicos. Pablo Gay estudio Sistemas 
+        y luego, se especializó en TICs, Programación Web y Programación de Dispositivos Electrónicos. Él es también
+        un autodidacta serial habiendo compartido sus conocimientos en varios emprendimientos de Asistencia Tecnológica
+        que lanzó como el último denominado: Servicios Digitales Educativos. Priorizando el entrenamiento a jóvenes, 
+        Pablo Gay ha dirigido y participado como asistente en varios proyectos institucionales de colegios 
+        secundarios que llegaron a instancias nacionales en las competencias nacionales de Feria de Ciencias.         
+        
+        El Email de Pablo Gay: gpabloandres@gmail.com 
+        El Github de Pablo Gay : https://github.com/gpabloandres
+        Todos mis enlaces: https://linktr.ee/gpabloandres
         """
  
-st.title("Murtaza's AI Bot")
+st.title("AI Bot de Pablo")
  
-user_question = st.text_input("Ask anything about me")
-if st.button("ASK", use_container_width=400):
-    prompt = persona +"Here is the question that the user asked: " +  user_question
+user_question = st.text_input("Pregunta algo sobre mi...")
+if st.button("PREGUNTA", use_container_width=400):
+    prompt = persona +"Aquí está la pregunta que el usuario hizo: " +  user_question
     response = model.generate_content(prompt)
     st.write(response.text)
  
@@ -56,30 +52,29 @@ st.title(" ")
  
 col1, col2 = st.columns(2)
 with col1:
-    st.subheader("Youtube Channel")
-    st.write("- Largest Computer Vision Channel")
-    st.write("- 400k+ Subscribers")
-    st.write("- Over 150 Free Tutorials")
-    st.write("- 15 Million+ Views")
-    st.write("- 1.5 Million Hours+ Watch time")
+    st.subheader("Mi Plataforma Online para Asistencias IT")
+    st.write("- Diversos cursos y tutoriales para asistir en Tecnologías")
+    st.write("- Más de 100 clientes")
+    st.write("- Asistencias offline, online o híbridas")
+    st.write("- Cientos de horas de videos")
  
 with col2:
-    st.video("https://youtu.be/BFlRmIvqwSA?si=a6qL3krtRgqVIKOZ")
+    st.video("https://youtu.be/ZZmnh65ZExc")
  
 st.title(" ")
  
-st.title("My Setup")
-st.image("images/setup.jpg")
+st.title("Mi Estudio")
+st.image("images/setup.jpeg")
  
  
 st.write(" ")
-st.title("My Skills")
+st.title("Mis habilidades")
 st.slider("Programming", 0, 100, 70)
-st.slider("Teaching", 0, 100, 85)
-st.slider("Robotics", 0, 100, 75)
+st.slider("Teaching", 0, 100, 95)
+st.slider("IOTs", 0, 100, 75)
  
 st.write(" ")
-st.title("Gallery")
+st.title("Galería")
  
 col1, col2, col3 = st.columns(3)
  
@@ -93,12 +88,7 @@ with col2:
     st.image("images/g5.jpg")
     st.image("images/g6.jpg")
  
-with col3:
-    st.image("images/g7.jpg")
-    st.image("images/g8.jpg")
-    st.image("images/g9.jpg")
- 
 st.subheader(" ")
-st.write("CONTACT")
-st.title("For any inquiries, email at:")
-st.subheader("contact@murtazahassan.com")
+st.write("CONTACTO")
+st.title("Quedo atento a tus consultas vía email a:")
+st.subheader("gpabloandres@gmail.com")
